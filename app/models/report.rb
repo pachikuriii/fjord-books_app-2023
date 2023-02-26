3c05a91bcfa2010f133f29439879361ed19df69e
pachikuriii <stakahashi0728@gmail.com>
2023-02-26 10:50:26 +0900

    関連付け追加

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,6 +3,21 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
+  has_many :active_mentions,
+           class_name: 'Mention',
+           foreign_key: 'mentioning_report_id',
+           dependent: :destroy,
+           inverse_of: :mentioning_reports
+  has_many :passive_mentions,
+           class_name: 'Mention',
+           foreign_key: 'mentioned_report_id',
+           dependent: :destroy,
+           inverse_of: :mentioned_reports
+  has_many :mentioning_reports,
+           through: :active_mentions
+  has_many :mentioned_reports,
+           through: :passive_mentions
+
 
   validates :title, presence: true
   validates :content, presence: true
